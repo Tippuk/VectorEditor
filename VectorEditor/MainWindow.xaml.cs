ef363877a6ed72700f1973c73da5bd7d902fcd02
Илья Ярмульский <ilaarmulskij119@gmail.com>
2025-10-18 11:41:13 +1000

    коммит 2

--- VectorEditor/MainWindow.xaml.cs
+++ VectorEditor/MainWindow.xaml.cs
@@ -1,12 +1,33 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using VectorEditor.Models;
 
 namespace VectorEditor
 {
     public partial class MainWindow : Window
     {
+        private List<BaseShape> shapes;
+
         public MainWindow()
         {
             InitializeComponent();
+            shapes = new List<BaseShape>();
+            RedrawCanvas();
+        }
+
+        private void RedrawCanvas()
+        {
+            DrawingCanvas.Children.Clear();
+            foreach (var shape in shapes)
+            {
+                Shape ui = shape.ToUIElement();
+                Canvas.SetLeft(ui, shape.X);
+                Canvas.SetTop(ui, shape.Y);
+                DrawingCanvas.Children.Add(ui);
+            }
         }
     }
 }
