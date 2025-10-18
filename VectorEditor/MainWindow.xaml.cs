463806d2653e4d51a6af6d611a026a2c81570ac8
ilaar <ilaar@KOLIBRY>
2025-10-18 10:56:32 +1000

    Добавьте файлы проекта.

---
+++ VectorEditor/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿// NEW: basic window code-behind
+using System.Windows;
+
+namespace VectorEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
